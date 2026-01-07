05f1cb01b638e82f1ce1bb5ae20726401d539b29
dotM <29275165+dotMortis@users.noreply.github.com>
2026-01-07 22:48:57 +0100

    add en and de key definitions for systems with german layout

---
+++ config/keys_de.h
@@ -0,0 +1,77 @@
+// German QWERTZ to US QWERTY key mappings
+
+#define KEY_Q Q
+#define KEY_W W
+#define KEY_E E
+#define KEY_R R
+#define KEY_T T
+#define KEY_Z Y
+#define KEY_U U
+#define KEY_I I
+#define KEY_O O
+#define KEY_P P
+#define KEY_A A
+#define KEY_S S
+#define KEY_D D
+#define KEY_F F
+#define KEY_G G
+#define KEY_H H
+#define KEY_J J
+#define KEY_K K
+#define KEY_L L
+#define KEY_Y Z
+#define KEY_X X
+#define KEY_C C
+#define KEY_V V
+#define KEY_B B
+#define KEY_N N
+#define KEY_M M
+
+// umlauts
+#define KEY_OE SEMICOLON // Ö
+#define KEY_UE LEFT_BRACKET // Ü
+#define KEY_AE SINGLE_QUOTE // Ä
+#define KEY_SZ MINUS // ß ? backslash
+
+// symbols
+#define KEY_ACCENT EQUAL // ´ ` 
+#define KEY_HASH BSLH // # '
+#define KEY_COMMA COMMA // , ;
+#define KEY_DOT DOT // . :
+#define KEY_CARET GRAVE// ^ °
+#define KEY_PLUS RIGHT_BRACKET // + * ~
+#define KEY_LT NUBS // < > |
+#define KEY_MINUS SLASH // - _
+
+// shifted symbols
+#define KEY_SECTION LS(N3)     // §
+#define KEY_EXCLAMATION LS(N1)   // !
+#define KEY_QUOTE LS(N2)       // "
+#define KEY_DOLLAR LS(N4)      // $
+#define KEY_PERCENT LS(N5)    // %
+#define KEY_AMPERSAND LS(N6)     // &
+#define KEY_SLASH LS(N7)       // /
+#define KEY_LEFT_PAREN LS(N8)  // (
+#define KEY_RIGHT_PAREN LS(N9) // )
+#define KEY_EQUAL LS(N0)       // =
+#define KEY_QUESTION LS(KEY_SZ) // ?
+#define KEY_BACKTICK LS(KEY_ACCENT) // `
+#define KEY_SQUOTE LS(KEY_HASH) // '
+#define KEY_SEMICOLON LS(COMMA) // ;
+#define KEY_COLON LS(DOT)// :
+#define KEY_STAR LS(KEY_PLUS) // *
+#define KEY_GT LS(KEY_LT) // >
+#define KEY_UNDERSCORE LS(KEY_MINUS) // _
+
+// alt symbols
+#define KEY_EURO RA(KEY_E)    // €
+#define KEY_AT RA(KEY_Q)     // @
+#define KEY_LEFT_BRACE RA(N7)  // {
+#define KEY_LEFT_BRACKET RA(N8) // [
+#define KEY_RIGHT_BRACKET RA(N9) // ]
+#define KEY_RIGHT_BRACE RA(N0)   // }
+#define KEY_PIPE RA(KEY_LT) // |
+#define KEY_TILDE RA(KEY_PLUS) // ~
+#define KEY_BACKSLASH RA(KEY_SZ) // backslash
+
+
---
+++ config/keys_en.h
@@ -0,0 +1,77 @@
+// German QWERTZ to US QWERTY key mappings
+
+#define KEY_Q Q
+#define KEY_W W
+#define KEY_E E
+#define KEY_R R
+#define KEY_T T
+#define KEY_Z Z
+#define KEY_U U
+#define KEY_I I
+#define KEY_O O
+#define KEY_P P
+#define KEY_A A
+#define KEY_S S
+#define KEY_D D
+#define KEY_F F
+#define KEY_G G
+#define KEY_H H
+#define KEY_J J
+#define KEY_K K
+#define KEY_L L
+#define KEY_Y Y
+#define KEY_X X
+#define KEY_C C
+#define KEY_V V
+#define KEY_B B
+#define KEY_N N
+#define KEY_M M
+
+// umlauts
+#define KEY_OE O // Ö
+#define KEY_UE U // Ü
+#define KEY_AE A // Ä
+#define KEY_SZ S // ß 
+
+// symbols
+#define KEY_ACCENT GRAVE // ´ ` 
+#define KEY_HASH HASH // # '
+#define KEY_COMMA COMMA // , ;
+#define KEY_DOT DOT // . :
+#define KEY_CARET CARET// ^ 
+#define KEY_PLUS PLUS // + 
+#define KEY_LT LT // < > |
+#define KEY_MINUS MINUS // - _
+
+// shifted symbols
+#define KEY_SECTION LALT(N3)     // §
+#define KEY_EXCLAMATION EXCLAMATION   // !
+#define KEY_QUOTE DQT       // "
+#define KEY_DOLLAR DOLLAR      // $
+#define KEY_PERCENT PRECENT    // %
+#define KEY_AMPERSAND AMPS     // &
+#define KEY_SLASH SLASH       // /
+#define KEY_LEFT_PAREN LPAR  // (
+#define KEY_RIGHT_PAREN RPAR // )
+#define KEY_EQUAL EQUAL       // =
+#define KEY_QUESTION QMARK // ?
+#define KEY_BACKTICK GRAVE // `
+#define KEY_SQUOTE SQT // '
+#define KEY_SEMICOLON SEMI // ;
+#define KEY_COLON COLON// :
+#define KEY_STAR STAR // *
+#define KEY_GT GT // >
+#define KEY_UNDERSCORE UNDER // _
+
+// alt symbols
+#define KEY_EURO E    // €
+#define KEY_AT AT     // @
+#define KEY_LEFT_BRACE LBRC  // {
+#define KEY_LEFT_BRACKET LBKT // [
+#define KEY_RIGHT_BRACKET RBKT // ]
+#define KEY_RIGHT_BRACE RBRC   // }
+#define KEY_PIPE PIPE // |
+#define KEY_TILDE TILDE // ~
+#define KEY_BACKSLASH BSLH // backslash
+
+
